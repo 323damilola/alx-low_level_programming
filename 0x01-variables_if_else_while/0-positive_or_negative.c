cd1ddc69d45557e2522454f58afef94d87a11f8d
323damilola <damilolaogunbayo323@gmail.com>
2022-12-09 14:16:28 -0800

    NICE

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_Max / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n):
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
