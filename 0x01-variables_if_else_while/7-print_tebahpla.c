8396c756d94c7261e4590cb84db1210e592448ad
323damilola <damilolaogunbayo323@gmail.com>
2022-12-10 11:15:21 -0800

    Hoodie

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
