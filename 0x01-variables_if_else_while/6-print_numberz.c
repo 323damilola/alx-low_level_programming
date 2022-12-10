052635a25b190c0b0bb6beb8c23b740b930956f1
323damilola <damilolaogunbayo323@gmail.com>
2022-12-10 11:05:56 -0800

    Hoodie

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed bby new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
