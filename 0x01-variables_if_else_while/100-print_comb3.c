f0ff6db54e4206fa325287242b35f79f1233d207
323damilola <damilolaogunbayo323@gmail.com>
2022-12-11 00:14:17 -0800

    Hoodie

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: WWrite a program that prints all possible
+ * different ombination of two digits
+ * Numbers must be seperated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of two digits 0 and 1
+ * Print only the two smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (Every other function (printf, puts, etc...) is forbidden)
+ * You can only use putchar five times maximum in your code
+ *  You are not allowed to use any variable of type char
+ *  All your codes should be in the main function
+ *  Return o
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar('.');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
