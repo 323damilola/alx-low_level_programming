5543b84773cb0b11329349288cc73453e8833397
323damilola <damilolaogunbayo323@gmail.com>
2022-12-13 13:37:06 -0800

    Life Sweet

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print the last digit
+ * @d: The last digit of the number
+ * Return: Value of he last digit
+ */
+
+int print_last_digit(int d)
+{
+	int ld = % 10;
+
+	if (ld < o)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
