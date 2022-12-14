8a7293e40522c6ec5be6763501ab2a0d6d5b92f0
323damilola <damilolaogunbayo323@gmail.com>
2022-12-13 17:15:09 -0800

    Not Bad

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, " n--);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-		printf("%d, ", n);
+		printf("%d\n, ", n);
 	}
 }
