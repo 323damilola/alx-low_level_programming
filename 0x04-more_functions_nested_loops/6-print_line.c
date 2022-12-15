f6d2ce91bfbf34a78c8b698728162b5cde4f6eaa
323damilola <damilolaogunbayo323@gmail.com>
2022-12-15 14:25:29 -0800

    JUST DEY PLAY

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ * No even ask why i write am
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
