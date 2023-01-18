d780934ac8d271534283abc78434439430258775
323damilola <damilolaogunbayo323@gmail.com>
2023-01-18 14:31:43 -0800

    Very nice

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,3 +3,11 @@
 /**
  * print_name - this function prints a name
  * @name: pointer string
+ * @f: pointer to function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
