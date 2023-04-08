9c2391bcccc0efe9cf754ab01722500fc27806df
323damilola <damilolaogunbayo323@gmail.com>
2023-04-07 17:42:53 -0700

    It went well

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,8 @@
-#include "./hash_tables.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
- *
  * Description: Key/value pairs are printed in the order
  *              they appear in the array of the hash table.
  */
